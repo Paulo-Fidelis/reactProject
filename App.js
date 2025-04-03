31b6f29757865bb4c11a02c06225bd0b1ae658ba
DESKTOP-7M01SED\Aluno <paulofideli68@gmail.com>
2025-04-03 11:13:11 -0300

    projeto iniciado

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Input } from 'react-native-elements';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <AntDesign name="user" size={160} color="black"/>
+      <Text>Email</Text>
+      <Input></Input>
+      <Text>Senha</Text>
+      <Input></Input>
+      <View styles={styles.button}>
+        <Button
+          title="login"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"/>
+      </View>
+      <View styles={styles.button}>
+        <Button
+          title="cadastro"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"/>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+function Login(){
+  return (
+    <View style={styles.container}>
+      <AntDesign name="user" size={160} color="black"/>
+      <Text>Email</Text>
+      <Input></Input>
+      <Text>Senha</Text>
+      <Input></Input>
+      <View styles={styles.button}>
+        <Button
+          title="login"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"/>
+      </View>
+      <View styles={styles.button}>
+        <Button
+          title="cadastro"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +63,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    margin: 50,
+    padding: 50,
   },
 });
